b886c8cdff11073cce15896bd8d3b30b6046d736
Robert Vincent <robert.vincent@conning.com>
2019-08-01 08:15:02 -0400

    Separate password types in hiera.

--- types/user/resource.pp
+++ types/user/resource.pp
@@ -1,6 +1,7 @@
 type Account::User::Resource = Struct[
   { Optional[ensure]                   => Enum['absent','present'],
     Optional[comment]                  => String,
+    Optional[crypted_password]         => String,
     Optional[expire]                   => Account::Date,
     Optional[group]                    => Account::Name,
     Optional[groups]                   => Array[Account::Name],
@@ -8,7 +9,7 @@ type Account::User::Resource = Struct[
     Optional[home_source_module]       => String[1],
     Optional[key]                      => Stdlib::Base64,
     Optional[keytype]                  => Account::SshKeyType,
-    Optional[password]                 => String,
+    Optional[windows_password]         => String,
     Optional[shared_accounts]          => Array[Account::Name],
     Optional[shell]                    => Stdlib::Unixpath,
     Optional[uid]                      => Account::Id,
